c333adcd9e571eb0e76f1be1442a264bc5e541c2
VuVanLien1503 <117723698+VuVanLien1503@users.noreply.github.com>
2023-04-11 14:59:46 +0700

    API Login hoàn thành

--- TMDT-Server/src/main/java/com/example/tmdtserver/service/icore/ICrud.java
+++ TMDT-Server/src/main/java/com/example/tmdtserver/service/icore/ICrud.java
@@ -1,9 +1,14 @@
 package com.example.tmdtserver.service.icore;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-public interface ICrud <E>{
-    Page<E>listAll(Page page);
-    void save(E e);
+public interface ICrud<E> {
+    Page<E> listAll(Pageable pageable);
+
+    boolean save(String inputCode, E e);
+
     void delete(Long id);
+
+    String randomCode(E e);
 }
--- TMDT-Server/src/main/java/com/example/tmdtserver/service/impl/AccountServiceImpl.java
+++ TMDT-Server/src/main/java/com/example/tmdtserver/service/impl/AccountServiceImpl.java
@@ -1,30 +1,50 @@
 package com.example.tmdtserver.service.impl;
 
 import com.example.tmdtserver.model.Account;
+import com.example.tmdtserver.repository.IAccountRepository;
 import com.example.tmdtserver.service.EmailService;
 import com.example.tmdtserver.service.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
 
 @Service
 public class AccountServiceImpl implements IAccountService {
-
     @Autowired
     private EmailService emailService;
+    @Autowired
+    private IAccountRepository iAccountRepository;
 
     @Override
-    public Page<Account> listAll(Page page) {
-        return null;
+    public Page<Account> listAll(Pageable pageable) {
+        return iAccountRepository.findAll(pageable);
     }
 
     @Override
-    public void save(Account account) {
-        String to="";
-        String subject="";
-        String text="";
-        emailService.sendMail(to,subject,text);
+    public String randomCode(Account account) {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(900000) + 100000;
+        String to = account.getUsers().getEmail();
+        String subject = "Mã Xác Nhận Của Bạn";
+        String text = String.valueOf(randomNum);
+        emailService.sendMail(to, subject, text);
+        return text;
 
+    }
+
+    @Override
+    public boolean save( String inputCode, Account account) {
+        boolean check = true;
+        if (code.equals(inputCode)) {
+            iAccountRepository.save(account);
+        } else {
+            check = false;
+        }
+        return check;
     }
 
     @Override
